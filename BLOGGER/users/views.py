3118a8b9cef5529882b3f62989fa3d69fa28b9e8
praj000 <parajbhattacharjee123@gmail.com>
2020-07-18 14:06:45 +0530

    A

--- BLOGGER/users/views.py
+++ BLOGGER/users/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
-from .forms import UserRegisterForm
+from .forms import *
 def register(req):
     if req.method=="POST":
         form=UserRegisterForm(req.POST);
@@ -19,6 +18,21 @@ def register(req):
 
 @login_required
 def Profile(req):
-    return render(req,"users/profile.html");
+    if req.method=="POST":
+        f1=UserUpdateForm(req.POST,instance=req.user)
+        f2=AccountUpdateForm(req.POST,req.FILES,instance=req.user.account)
+        if f1.is_valid() and f2.is_valid():
+            f1.save()
+            f2.save()
+            return redirect("user-profile")
+    else:
+        f1=UserUpdateForm(instance=req.user)
+        f2=AccountUpdateForm(instance=req.user.account)
+        
+    context={
+        "f1":f1,
+        "f2":f2
+    }
+    return render(req,"users/profile.html",context);
 
     

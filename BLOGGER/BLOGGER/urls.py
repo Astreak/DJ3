657d0c019bf2919e56897f0ff4ac2ca23e87300c
praj000 <parajbhattacharjee123@gmail.com>
2020-07-18 13:34:00 +0530

    L

--- BLOGGER/BLOGGER/urls.py
+++ BLOGGER/BLOGGER/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('blog/',include("blog.urls")),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
